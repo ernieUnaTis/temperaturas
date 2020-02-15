83324a9ca30ffd224944d14a42dc0378936b503d
ernesto <ernesto.deunanuet@tutanota.com>
2020-02-15 15:09:29 -0600

    Version final

--- app/controllers/temperaturas_controller.rb
+++ app/controllers/temperaturas_controller.rb
@@ -5,16 +5,36 @@ class TemperaturasController < ApplicationController
   end
 
   def obtener_historicos
-    #@temperaturas = Temperatura.all
-    @filterrific = initialize_filterrific(
-    Temperatura,
-    params[:filterrific]
-  ) or return
-  @temperaturas = Temperatura.all
+    @temperaturas = Temperatura.all
+    #@filterrific = initialize_filterrific(
+    #Temperatura,
+    #params[:filterrific]
+  #) or return
+  #@temperaturas = Temperatura.all
 
-  respond_to do |format|
-    format.html
-    format.js
+  #respond_to do |format|
+  #  format.html
+  #  format.js
+  #end
   end
+
+  def obtener_historicos_por_fecha_registro
+    @temperaturas = Temperatura.order("registro asc")
+    render "obtener_historicos"
+  end
+
+  def obtener_historicos_por_fecha_registro_desc
+      @temperaturas = Temperatura.order("registro desc")
+        render "obtener_historicos"
+  end
+
+  def obtener_historicos_por_temperatura_desc
+    @temperaturas = Temperatura.order("temperatura desc")
+      render "obtener_historicos"
+  end
+
+  def obtener_historicos_por_temperatura
+      @temperaturas = Temperatura.order("temperatura asc")
+        render "obtener_historicos"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   get 'temperatura_mas_baja/' => 'temperaturas#temperatura_mas_baja'
   get 'obtener_historicos/' => 'temperaturas#obtener_historicos'
+    get 'obtener_historicos_por_fecha_registro/' => 'temperaturas#obtener_historicos_por_fecha_registro'
+      get 'obtener_historicos_por_fecha_registro_desc/' => 'temperaturas#obtener_historicos_por_fecha_registro_desc'
+        get 'obtener_historicos_por_temperatura_desc/' => 'temperaturas#obtener_historicos_por_temperatura_desc'
+          get 'obtener_historicos_por_temperatura/' => 'temperaturas#obtener_historicos_por_temperatura'
 end
